9d6aac97434d740874ec3f66773814ada1ed7304
251328109 <251328109@qq.com>
2018-05-06 16:24:10 +0800

    wen

--- server/http_server.php
+++ server/http_server.php
@@ -3,7 +3,7 @@
 $http = new swoole_http_server("0.0.0.0",9507);
 
 $http->set([
-   'enable_static_handler' => true,
+    'enable_static_handler' => true,
     'document_root' => '/var/www/html/swoole/swoole/public/static',
     'worker_num' => 5,
 ]);
@@ -12,7 +12,9 @@ $http->on('WorkerStart',function (swoole_server $server , $worker_id){
     // 定义应用目录
     define('APP_PATH', __DIR__ . '/../application/');
     // 加载框架引导文件
-    require __DIR__ . '/../thinkphp/base.php';
+//    require __DIR__ . '/../thinkphp/base.php';
+    require __DIR__ . '/../thinkphp/start.php';
+
 });
 
 $http->on('request', function ($request, $response) {
