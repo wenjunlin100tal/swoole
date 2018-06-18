f9159fa530a28a55265a87f0cb5c0d9036850162
251328109 <251328109@qq.com>
2018-06-18 23:04:18 +0800

    w

--- server/http_server.php
+++ server/http_server.php
@@ -57,7 +57,7 @@ $http->on('request', function ($request, $response) use($http){
     ob_end_clean();
 //    $response->cookie('singwa','xssss',time()+1800);
     $response->end($res);
-    $ret = $http->close($request->fd,true);
+    $ret = $http->close($request->fd);
     if($ret){
         echo 1;
     }else{
