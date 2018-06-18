2539fa40374d2f02cce2546647ca0ca492a40b0d
251328109 <251328109@qq.com>
2018-06-18 22:40:28 +0800

    w

--- server/http_server.php
+++ server/http_server.php
@@ -41,7 +41,7 @@ $http->on('request', function ($request, $response) use($http){
             $_POST[$k] = $v;
         }
     }
-//    echo "action:".request()->action();
+    echo "action:".request()->action().PHP_EOL;
     ob_start();
     // 执行应用并响应
     try{
