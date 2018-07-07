e815d007e1069519db3807e059951f5b00da865f
251328109 <251328109@qq.com>
2018-07-07 22:20:56 +0800

    sms

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -20,6 +20,7 @@ class Index
 
     public function sms()
     {
+        header("Content-type: text/html; charset=utf-8");
         $phone = 17784496304;
         $code = 123456;
         $result = Sms::sendSms($phone, $code);
