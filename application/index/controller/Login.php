9155e0adf396e3dfa8038bfc4f5b07fdc02a78d1
936968629 <936968629@qq.com>
2018-07-12 23:36:40 +0800

    wen

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -13,7 +13,7 @@ class Login extends Controller
         $phoneNum = intval($_GET['phone_num']);
         $code = intval($_GET['code'] );
         echo 2;
-        return;
+        return 55;
         if(empty($phoneNum) || empty($code) ){
             return Util::show(config('code.error'),'error');
         }
